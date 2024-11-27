da634c0afc79cef2614b6aba0a4b3653aeefee40
Ernesto Márquez Guevara <Netomg2509@gmail.com>
2024-11-26 21:03:18 -0700

    a

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -230,7 +230,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(21, 715);
+            this.button1.Location = new System.Drawing.Point(22, 715);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(110, 23);
             this.button1.TabIndex = 20;
