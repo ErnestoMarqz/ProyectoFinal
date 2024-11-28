35844866750ad2189927aaa75a2e962bcbad0545
Ernesto Márquez Guevara <Netomg2509@gmail.com>
2024-11-27 20:49:05 -0700

    erika update 2

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,14 @@ namespace ProyectoFinal
 {
     public partial class Form1 : Form
     {
+        int[] arreglo;
         public Form1()
         {
             InitializeComponent();
+            //poner el forms en el centro de la pantalla
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(250, 10); // Cambia a la posición deseada
+
         }
 
         private void groupBox1_Enter(object sender, EventArgs e)
@@ -26,5 +31,43 @@ namespace ProyectoFinal
         {
 
         }
+
+        private void BTNCrear_Click(object sender, EventArgs e)
+        {
+            // Limpiar el FlowLayoutPanel antes de agregar nuevos cuadros
+            flowLayoutPanel1.Controls.Clear();
+
+            // Obtener el número ingresado por el usuario
+            int cantidad = (int)numericUpDown1.Value;
+            arreglo = new int[cantidad];
+            // Crear una lista de números y desordenarlos
+            Random random = new Random();
+            var numeros = Enumerable.Range(1, cantidad).OrderBy(x => random.Next()).ToList();
+            for (int i = 0; i < cantidad; i++)
+            {
+                arreglo[i] = numeros[i];
+            }
+            // Crear y agregar los cuadros al FlowLayoutPanel
+            Cuadritos creador = new Cuadritos();
+            foreach (int numero in numeros)
+            {
+                flowLayoutPanel1.Invalidate();
+                var cuadro = creador.CrearCuadroAnimadoEnLayout(flowLayoutPanel1, numero);
+                flowLayoutPanel1.Controls.Add(cuadro);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Creado por el equipo: NyE Squad");
+            Application.Exit();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
